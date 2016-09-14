8bb0e81699a989ca5510169d9a24e587d23f7df7
Jazmmine <jazmine.angoma@gmail.com>
2016-09-14 14:21:33 -0500

    modificando comillas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-document.getElementById('form-signin-heading').innerText='Por favor inicia sesión';
-document.getElementById('inputEmail').placeholder='Correo Electrónico';
-document.getElementById('inputPassword').placeholder='Contraseña';
-document.querySelector('span').innerText='Recordar datos';
-document.querySelector('.btn').innerText='Iniciar Sesión';
+document.getElementById("form-signin-heading").innerText="Por favor inicia sesión";
+document.getElementById("inputEmail").placeholder="Correo Electrónico";
+document.getElementById("inputPassword").placeholder="Contraseña";
+document.querySelector("span").innerText="Recordar datos";
+document.querySelector("button").innerText="Iniciar Sesión";
