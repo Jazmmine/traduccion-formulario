caecdcb54f405676f4eeb2008be0a34de88785e5
Jazmmine <jazmine.angoma@gmail.com>
2016-09-14 14:06:09 -0500

    modificando a espaniol

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById('form-signin-heading').innerText='Por favor inicia sesión';
+document.getElementById('inputEmail').placeholder='Correo Electrónico';
+document.getElementById('inputPassword').placeholder='Contraseña';
+document.querySelector('span').innerText='Recordar datos';
+document.querySelector('.btn').innerText='Iniciar Sesión';
